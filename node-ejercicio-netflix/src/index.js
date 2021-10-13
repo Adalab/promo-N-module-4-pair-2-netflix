dee714cf315f05521f2ba05999c19f1e6ef7b4cf
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-13 11:08:56 +0200

    exercise day 3

--- node-ejercicio-netflix/src/index.js
+++ node-ejercicio-netflix/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const movies = require('./data/movies.json');
+const path = require('path');
 
 // create and config server
 const server = express();
@@ -20,4 +21,18 @@ server.get('/movies', (req, res) => {
     movies,
   };
   res.json(response);
+});
+
+const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb));
+
+// Endpoint para gestionar los errores 404
+server.get('*', (req, res) => {
+  // Relativo a este directorio
+  const notFoundFileRelativePath = '../public-react/404-not-found.html';
+  const notFoundFileAbsolutePath = path.join(
+    __dirname,
+    notFoundFileRelativePath
+  );
+  res.status(404).sendFile(notFoundFileAbsolutePath);
 });
