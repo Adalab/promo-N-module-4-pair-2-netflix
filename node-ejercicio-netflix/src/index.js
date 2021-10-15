2389c8fc5b3fb02835d049abe74ded2f3d494605
mdanieladla <mariadanieladarnea@gmail.com>
2021-10-15 11:35:33 +0200

    addedexercise day 4

--- node-ejercicio-netflix/src/index.js
+++ node-ejercicio-netflix/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const movies = require('./data/movies.json');
 const path = require('path');
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
@@ -14,13 +15,19 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+//configuramos base de datos
+const db = new Database('./src/db/database.db', {
+  verbose: console.log,
+});
+
 // Escribimos los endpoints que queramos
 server.get('/movies', (req, res) => {
-  const response = {
-    success: true,
-    movies,
-  };
-  res.json(response);
+  const query = db.prepare('SELECT * FROM movies');
+  const movies = query.all();
+  console.log(movies);
+  res.json({
+    movies: movies,
+  });
 });
 server.get('/movie/:movieId', (req, res) => {
   console.log('url=', req.params);
